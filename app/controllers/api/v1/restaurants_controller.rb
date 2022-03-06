327fa991b22fdb1181dbc5f046e791fee5db09c9
JAMChannel <pon244tt@gmail.com>
2022-03-06 14:20:06 +0900

    controller追加

---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::RestaurantsController < ApplicationController
+  def index
+    restaurants = Restaurant.all
+    render json: { restaurants: restaurants }, status: :ok
+  end
+end
+
+
+# module Api
+#   module V1
+#     class RestaurantsController < ApplicationController
+#       def index
+#         restaurants = Restaurant.all
+
+#         render json: {
+#           restaurants: restaurants
+#         }, status: :ok
+#       end
+#     end
+#   end
+# end
